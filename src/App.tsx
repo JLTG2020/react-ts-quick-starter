b80bb5e5f6060b911d88b8e0e240d6f4446d878a
aleiji <aleiji@tencent.com>
2021-06-16 08:42:42 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from 'Components/Header';
 import './App.scss';
+import { BABYLON, DefaultViewer } from 'babylonjs-viewer';
 
 interface IProps {
   name: string;
@@ -8,11 +9,37 @@ interface IProps {
 }
 
 function App(props: IProps) {
+  const config = {
+    extends: 'extended',
+    configuration: './config.json',
+    model: {
+      url: 'https://babylonjs.com/Assets/FlightHelmet/glTF/FlightHelmet_Materials.gltf',
+    },
+    templates: {
+      main: {
+        params: {
+          fillScreen: true,
+        },
+      },
+    },
+    lab: {
+      assetsRootURL: '/external/msft/',
+    },
+  };
+
+  const viewerElement = document.querySelector('#viewport');
+  let dv;
+  if (viewerElement !== null) {
+    dv = new DefaultViewer(viewerElement, config);
+  }
   const { name, age } = props;
   return (
     <div className='app'>
       <Header />
       <span>{`Hello! I'm ${name}, ${age} yearssss old.`}</span>
+      <div>
+        <div id='viewport' />
+      </div>
     </div>
   );
 }
